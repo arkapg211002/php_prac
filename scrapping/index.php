cdf6593b4168ec301bb1b3cc4edf576ac06f3c59
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2022-12-15 22:20:12 +0530

    Add files via upload

---
+++ scrapping/index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<style>
+    .iframeContainer {
+        position: relative;
+        width: 1158px;
+        height: 153px;
+        overflow: hidden;
+    }
+
+    .iframeContent {
+        position: absolute;
+        top: -1216px;
+        left: -170px;
+        width: 1500px;
+        height: 2000px;
+    }
+
+    .jjContainer{
+        position: relative;
+        width: 1158px;
+        height: 153px;
+        overflow: hidden;
+    }
+
+    .jjContent {
+        position: absolute;
+        top: -123px;
+        left: -170px;
+        width: 1500px;
+        height: 2000px;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <h1>vickygold</h1>
+        <div class="iframeContainer">
+            <iframe src="https://www.vickygold.in/" scrolling="no" class="iframeContent"></iframe>
+        </div>
+        <a href="https://www.vickygold.in/">Vicky gold</a>
+
+
+    </div>
+    </br>
+    </br>
+    </br>
+
+    <div class="container">
+        <h1>jj gold house</h1>
+        <div class="jjContainer">
+            <iframe src="//www.jjgoldhouse.in/" scrolling="no" class="jjContent"></iframe>
+        </div>
+        <a href="http://www.jjgoldhouse.in/">jjgold house</a>
+
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
